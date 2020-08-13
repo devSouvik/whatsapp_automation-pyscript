c13252dcf20cf29931dfd6c11df599696e9f31b1
Souvik Guria <54489090+devSouvik@users.noreply.github.com>
2020-08-13 11:39:15 +0530

    Create whatsapp.py

---
+++ whatsapp.py
@@ -0,0 +1,13 @@
+import pywhatkit as kit
+
+// step 1- -> pip install pywhatkit <- in your IDE terminal or if you are using anaconda notepad write -> conta install pywhatkit
+
+// step 2 -> open whatsapp web in your preferred browser and be logged in with your whatsapp account
+
+// step 3-> schedule a message with a minumum time of 7 minutes from the execution of the script
+
+
+// fromat --->>> kit.sendwhatmsg("phone number with +91","text message ",hour(in 24 hrs format),minutes) 
+
+// example ->  kit.sendwhatmsg("+91xxxxxxxxxx","good night ",23,35)
+
