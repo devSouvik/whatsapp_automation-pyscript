842fa57477caf6c3b2f34832375e6efb0d943eec
Souvik Guria <54489090+devSouvik@users.noreply.github.com>
2020-08-13 13:47:46 +0530

    Update whatsapp.py

--- whatsapp.py
+++ whatsapp.py
@@ -10,9 +10,10 @@ import pywhatkit as kit
 
 #  step 3-> schedule a message with a minumum time of 7 minutes from the execution of the script
 
-#  step 4->  if you want to multiple messages from 1 script...don't forget to give  a time diff of minimum 5 minutes from the previous message....
+#  step 4->  if you want to send multiple messages from 1 script...don't forget to give  a time diff of minimum 5 minutes from the previous message....
 
-#  fromat --->>> kit.sendwhatmsg("phone number with +91","text message ",hour(in 24 hrs format),minutes) 
+
+#  format --->>> kit.sendwhatmsg("phone number with +91","text message ",hour(in 24 hrs format),minutes) 
 
 #  example ->  kit.sendwhatmsg("+91xxxxxxxxxx","good night ",23,35)
 
